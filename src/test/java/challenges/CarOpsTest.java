6ced02c8acc719f42cf41baa7e12ede8a852af05
Donato Manzione <donatousc@hotmail.com>
2023-10-15 21:51:58 -0400

    set up for testing

---
+++ src/test/java/challenges/CarOpsTest.java
@@ -0,0 +1,103 @@
+package challenges;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarOpsTest {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void filterByMake() {
+    }
+
+    @Test
+    void filterByYear() {
+    }
+
+    @Test
+    void filterByPrice() {
+    }
+
+    @Test
+    void mapToModelNames() {
+    }
+
+    @Test
+    void mapToUppercases() {
+    }
+
+    @Test
+    void sortByYear() {
+    }
+
+    @Test
+    void sortByPrice() {
+    }
+
+    @Test
+    void getHighestPricedCar() {
+    }
+
+    @Test
+    void getLowestPricedCar() {
+    }
+
+    @Test
+    void groupByMake() {
+    }
+
+    @Test
+    void mapToCarMake() {
+    }
+
+    @Test
+    void countCarsByMake() {
+    }
+
+    @Test
+    void averagePrice() {
+    }
+
+    @Test
+    void sumOfPrices() {
+    }
+
+    @Test
+    void anyCarWithBlueColor() {
+    }
+
+    @Test
+    void allCarsAreExpensive() {
+    }
+
+    @Test
+    void noneMatchTheCondition() {
+    }
+
+    @Test
+    void skipTheFirstNCars() {
+    }
+
+    @Test
+    void limitToNCars() {
+    }
+
+    @Test
+    void distinctColors() {
+    }
+
+    @Test
+    void concatenateMakeAndModel() {
+    }
+}
