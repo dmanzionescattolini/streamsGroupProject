edf86f4adabcff0e885e0572200192f670fdce47
qwqw1111 <haycockyoung@gmail.com>
2023-10-16 01:56:12 -0400

    first 5 done

--- src/main/java/challenges/StudentOps.java
+++ src/main/java/challenges/StudentOps.java
@@ -2,12 +2,38 @@ package challenges;
 
 import domain.Student;
 
+import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StudentOps {
+	
+	//Q1
     public static List<Student> filterStudentsByGender(List<Student> students, String gender) {
         return students.stream()
                 .filter(student -> student.getGender().equalsIgnoreCase(gender))
                 .toList();
+    }
+    
+    //Q2
+    public static List<Student> sortStudentsByAge(List<Student> students){
+    	return students.stream().sorted(Comparator.comparing(Student::getDob)).toList();
+    }
+    
+    //Q3
+    public static double calculateAverageAge(List<Student> students){
+    	return students.stream().map(s->s.getDob().until(LocalDate.now())).collect(Collectors.averagingDouble(d->d.getYears()*12+d.getMonths()))/12;
+    }
+    
+    //Q4
+    public static void printStudentNames(List<Student> students) {
+    	students.stream().forEach(s->System.out.println(s.getFirst_name()+" "+s.getLast_name()));
+    }
+    
+    //Q5
+    public static Map<String, List<Student>> groupStudentsByGender(List<Student> students){
+    	return students.stream().collect(Collectors.groupingBy(Student::getGender));
     }
 }
--- src/test/java/data/StudentOpsTest.java
+++ src/test/java/data/StudentOpsTest.java
@@ -2,7 +2,10 @@ package data;
 
 import challenges.StudentOps;
 import domain.Student;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,7 +13,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,13 +25,18 @@ public class StudentOpsTest {
 
         private static List<Student> students;
 
-        @BeforeAll
-        static void setUp() throws IOException {
+        @BeforeEach
+        void setUp() throws IOException {
             // Fetch data before all tests
             students = FetchData.getStudentList();
         }
 
-
+        @AfterEach
+        void tearDown() {
+        	students = null;
+        }
+        
+        //Q1
 
         @Test
         void shouldReturnEmptyListWhenNoStudentsMatchGender() {
@@ -37,6 +48,87 @@ public class StudentOpsTest {
         void shouldReturnListStudentsMatchingGender() {
             List<Student> filteredStudents = StudentOps.filterStudentsByGender(students, "Female");
             assertEquals("Female",filteredStudents.get(5).getGender());
+        }
+        
+        //Q2
+        
+        @Test
+        void shouldReturnListStudentsSortedByBirthday() {
+        	List<Student> sortedStudents = StudentOps.sortStudentsByAge(students);
+        	for(int i = 0; i<sortedStudents.size()-1; i++) {
+        		assertTrue(sortedStudents.get(i).getDob().compareTo(sortedStudents.get(i+1).getDob())<=0);
+        	}
+        }
+        
+        //Q3
+        
+        @Test
+        void shouldReturnCorrectAverageAge() {
+        	double avg = StudentOps.calculateAverageAge(students);
+        	double sum = 0;
+        	Period age;
+        	for(Student s:students) {
+        		age = s.getDob().until(LocalDate.now());
+        		sum += age.getYears()*12 + age.getMonths();
+        	}
+        	sum /= students.size();
+        	sum /= 12;
+        	assertEquals(sum,avg);
+        }
+        
+        //Q4
+        @Test
+        void firstStudentNamePrintedCorrectly() {
+        	
+        	//https://stackoverflow.com/questions/21913408/reading-system-out-java-test
+        	java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream(); 
+        	java.io.PrintStream temp = System.out;   
+        	System.setOut(new java.io.PrintStream(out));
+        	
+        	StudentOps.printStudentNames(students);
+        	assertTrue(out.toString().contains("Kaitlyn Padden"));
+        	
+        	System.setOut(temp);
+        	
+        }
+        
+
+        @Test
+        void allStudentNamePrintedCorrectly() {
+        	
+        	//https://stackoverflow.com/questions/21913408/reading-system-out-java-test
+        	java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream();
+        	java.io.PrintStream temp = System.out;
+        	System.setOut(new java.io.PrintStream(out));
+        	StringBuilder nameList =new StringBuilder();
+        	
+        	StudentOps.printStudentNames(students);
+        	
+        	char problem = out.toString().charAt(14);
+        	
+        	for(Student s:students) {
+        		nameList.append(s.getFirst_name());
+        		nameList.append(" ");
+        		nameList.append(s.getLast_name());
+        		nameList.append(problem);
+        		nameList.append('\n');
+        	}
+        	
+        	assertTrue(nameList.toString().equals(out.toString()));
+        	
+        	
+        	System.setOut(temp);
+        }
+        
+        //Q5
+        @Test
+        void shouldReturnStudentsGroupedByGender() {
+        	Map<String,List<Student>> groups = StudentOps.groupStudentsByGender(students);
+        	for(String g: groups.keySet()) {
+        		for(Student s:groups.get(g)) {
+        			assertEquals(s.getGender(),g);
+        		}
+        	}
         }
     }
 
