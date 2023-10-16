2489c5a1b7f99ff016290bda584b0c6b320c80df
Donato Manzione <donatousc@hotmail.com>
2023-10-15 20:20:50 -0400

    all questions

--- src/main/java/challenges/CarOps.java
+++ src/main/java/challenges/CarOps.java
@@ -2,31 +2,14 @@ package challenges;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import data.FetchData;
 import domain.Car;
-import util.ComparatorCarsByPriceAscending;
-import util.ComparatorCarsByPriceDescending;
-import util.ComparatorCarsByYearAscending;
 
 public class CarOps {
-    public static void main(String[] args) throws IOException {
-       List<List<Car>> cars = CarOps.partitionCarsIntoAboveAndBelowNPrice(FetchData.getCarList(), 50_000);
-        System.out.println(cars.get(0));
-        System.out.println(cars.get(1));
-    }
-    //	41.Find First Car: Find the first car in the list.
-//	42.Find Any Car: Find any car in the list.
-//	43.Remove Duplicates: Remove duplicate cars from the list based on make and model.
-//	44.Partition Cars by Price: Partition the cars into two groups based on whether their price is above a certain value.
-//	45.Calculate Total Price by Make: Calculate the total price of cars for each make.
-//	46.Join Car Names into a String: Join the names of all cars into a single comma-separated string.
-//	47.Peek and Print: Use peek to print the details of each car in the stream.
-//	48.Average Price by Make: Calculate the average price of cars for each make.
-//	49.Concatenate All Car Details: Concatenate all car details into a single string.
-//	50.Find the Newest Car: Find the newest (latest year) car in the list.
+
 
     public static Car findFirstCar(List<Car> cars){
         return cars.stream().findFirst().orElseThrow();
@@ -52,9 +35,44 @@ public class CarOps {
         return Arrays.asList(cars.stream().filter(x->x.getPrice()<=N).toList(),cars.stream().filter(x->x.getPrice()>N).toList());
     }
 
-
-    public static calculateTotalPriceByMake(List<Car> cars){
-        Map<String, >
+    public static Map<String,List<Car>> groupByMake(List<Car> cars){
+        return cars.stream().collect(Collectors.groupingBy(x->x.getMake()));
+    }
+    public static double sumOfPrices(List<Car> cars){
+        return cars.stream().mapToDouble(c->c.getPrice()).reduce((x,y)-> x+y).getAsDouble();
+    }
+    public static Map<String, Double> calculateTotalPriceByMake(List<Car> cars){
+            Map<String, List<Car>> carsByMake = groupByMake(cars);
+            Map<String, Double> totalPriceByMake = new HashMap<>();
+            carsByMake.forEach((make, carz)->{
+                totalPriceByMake.put(make, sumOfPrices(carz));
+            });
+            return totalPriceByMake;
     }
 
+    public static String joinCarNamesIntoAString(List<Car> cars){
+        return cars.stream().map(car-> car.getMake() + " " + car.getModel() + " " + car.getYear()).distinct().reduce((acc, car)->acc + ", " + car).orElse("EMPTY");
+    }
+
+    public static List<Car> peekAndPrint(List<Car> cars){
+       return cars.stream().peek(x-> System.out.println(x)).toList();
+    }
+
+    public static Map<String,Double> averagePricePerMake(List<Car> cars){
+        Map<String, Double> averagePrices = new HashMap<>();
+        groupByMake(cars).forEach((make, carz)-> {
+            averagePrices.put(make, carz.stream().map(x -> x.getPrice()).reduce((acc, next) -> (acc + next) / 2).orElse(0.00));
+        });
+                        return averagePrices;
+        };
+
+
+    public static String concatenateAllCarDetails(List<Car> cars){
+        return cars.stream().map(x->x.toString()).reduce((acc, next)->acc+ "\n" + next.toString() ).orElse("EMPTY");
+    }
+
+    public static Car findNewestCar(List<Car> cars){
+        int earliestYear = cars.stream().mapToInt(x->x.getYear()).max().getAsInt();
+        return cars.stream().filter(x->x.getYear()==earliestYear).findFirst().get();
+    }
 }
